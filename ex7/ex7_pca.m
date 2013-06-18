c4e7fcad8c0df1b2ee0dda27001de2173d4adfa7
mofas <fedt1234@hotmail.com>
2013-06-18 10:27:43 +0800

    ex8 finish

--- ex7/ex7_pca.m
+++ ex7/ex7_pca.m
@@ -76,21 +76,21 @@ load ('ex7data1.mat');
 %
 %  You should complete the code in projectData.m
 %
-fprintf('\nDimension reduction on example dataset.\n\n');
+% fprintf('\nDimension reduction on example dataset.\n\n');
 
 %  Plot the normalized dataset (returned from pca)
 % plot(X_norm(:, 1), X_norm(:, 2), 'bo');
 % axis([-4 3 -4 3]); axis square
 
 %  Project the data onto K = 1 dimension
-K = 1;
-Z = projectData(X_norm, U, K);
-fprintf('Projection of the first example: %f\n', Z(1));
-fprintf('\n(this value should be about 1.481274)\n\n');
+% K = 1;
+% Z = projectData(X_norm, U, K);
+% fprintf('Projection of the first example: %f\n', Z(1));
+% fprintf('\n(this value should be about 1.481274)\n\n');
 
-X_rec  = recoverData(Z, U, K);
-fprintf('Approximation of the first example: %f %f\n', X_rec(1, 1), X_rec(1, 2));
-fprintf('\n(this value should be about  -1.047419 -1.047419)\n\n');
+% X_rec  = recoverData(Z, U, K);
+% fprintf('Approximation of the first example: %f %f\n', X_rec(1, 1), X_rec(1, 2));
+% fprintf('\n(this value should be about  -1.047419 -1.047419)\n\n');
 
 %  Draw lines connecting the projected points to the original points
 % hold on;
@@ -122,8 +122,8 @@ load ('ex7faces.mat')
 %  Run PCA and visualize the eigenvectors which are in this case eigenfaces
 %  We display the first 36 eigenfaces.
 %
-fprintf(['\nRunning PCA on face dataset.\n' ...
-         '(this mght take a minute or two ...)\n\n']);
+% fprintf(['\nRunning PCA on face dataset.\n' ...
+%         '(this mght take a minute or two ...)\n\n']);
 
 %  Before running PCA, it is important to first normalize X by subtracting 
 %  the mean value from each feature
@@ -133,50 +133,50 @@ fprintf(['\nRunning PCA on face dataset.\n' ...
 [U, S] = pca(X_norm);
 
 %  Visualize the top 36 eigenvectors found
-displayData(U(:, 1:36)');
+% displayData(U(:, 1:36)');
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+% fprintf('Program paused. Press enter to continue.\n');
+% pause;
 
 
 %% ============= Part 6: Dimension Reduction for Faces =================
 %  Project images to the eigen space using the top k eigenvectors 
 %  If you are applying a machine learning algorithm 
-fprintf('\nDimension reduction for face dataset.\n\n');
+% fprintf('\nDimension reduction for face dataset.\n\n');
 
 K = 100;
 Z = projectData(X_norm, U, K);
 
-fprintf('The projected data Z has a size of: ')
-fprintf('%d ', size(Z));
+% fprintf('The projected data Z has a size of: ')
+% fprintf('%d ', size(Z));
 
-fprintf('\n\nProgram paused. Press enter to continue.\n');
-pause;
+% fprintf('\n\nProgram paused. Press enter to continue.\n');
+% pause;
 
 %% ==== Part 7: Visualization of Faces after PCA Dimension Reduction ====
 %  Project images to the eigen space using the top K eigen vectors and 
 %  visualize only using those K dimensions
 %  Compare to the original input, which is also displayed
 
-fprintf('\nVisualizing the projected (reduced dimension) faces.\n\n');
+% fprintf('\nVisualizing the projected (reduced dimension) faces.\n\n');
 
 K = 100;
 X_rec  = recoverData(Z, U, K);
 
 % Display normalized data
-subplot(1, 2, 1);
-displayData(X_norm(1:100,:));
-title('Original faces');
-axis square;
+% subplot(1, 2, 1);
+% displayData(X_norm(1:100,:));
+% title('Original faces');
+% axis square;
 
 % Display reconstructed data from only k eigenfaces
-subplot(1, 2, 2);
-displayData(X_rec(1:100,:));
-title('Recovered faces');
-axis square;
+% subplot(1, 2, 2);
+% displayData(X_rec(1:100,:));
+% title('Recovered faces');
+% axis square;
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+% fprintf('Program paused. Press enter to continue.\n');
+% pause;
 
 
 %% === Part 8(a): Optional (ungraded) Exercise: PCA for Visualization ===
